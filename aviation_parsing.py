2bc8c232da5b09d660ac46fcc64dce9877dd1a05
Franklin Bradfield <fbradfield1@gmail.com>
2017-06-22 21:46:43 +0800

    Cleaned up CSV function by creating helper functions
    * removed redudant code from _extract_html; made names more accurate
    * _parse_html_request now handles all initial row cleaning
    * additional requests can be passed in a list to additional_requests argument
    * _parse_indexes allows for a one time extract of datetime indexes
    * multiple calls to _parse_data can be made in the case of additional requests
    * exceptions are raised for invalid airports, airlines, and requests
    * an existing 'aviation_data' directory is not required to run the function; it is created on command
    * no assignment is necessary if function is run with create_file=True; CSV file is created instead

--- aviation_parsing.py
+++ aviation_parsing.py
@@ -7,25 +7,22 @@ import json
 import numpy as np
 import pandas as pd
 import pickle
+import os
 import requests
 
 datadir = "aviation_data"
 
-def _extract_html(airline, airport, additional_requests):
+def _extract_html(airline, airport, additional_requests=None):
     
     session = requests.Session()
     get_request = session.get("https://www.transtats.bts.gov/Data_Elements.aspx?%2fData=2")
     
     soup = BeautifulSoup(get_request.text, 'lxml')
-    ev = soup.find(id="__EVENTVALIDATION")
-    vs = soup.find(id="__VIEWSTATE")
-    vsg = soup.find(id="__VIEWSTATEGENERATOR")
+    event_validation = soup.find(id="__EVENTVALIDATION")['value']
+    view_state = soup.find(id="__VIEWSTATE")['value']
+    view_state_generator = soup.find(id="__VIEWSTATEGENERATOR")['value']
     
-    event_validation = ev["value"]
-    view_state = vs["value"]
-    view_state_generator = vsg["value"]
-    
-    requests_html = []
+    html_requests = []
         
     passengers_request = session.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2",
                          data = (
@@ -39,7 +36,7 @@ def _extract_html(airline, airport, additional_requests):
                               ("Submit", "Submit")
                               ))
     
-    requests_html.append(passengers_request.text)
+    html_requests.append(passengers_request.text)
     
     if additional_requests:
         for request in additional_requests:
@@ -54,9 +51,9 @@ def _extract_html(airline, airport, additional_requests):
                               ("AirportList", airport),
                               ))
         
-            requests_html.append(request.text)
+            html_requests.append(request.text)
     
-    return requests_html
+    return html_requests
 
 
 def _parse_html_request(html_request):
@@ -70,6 +67,12 @@ def _parse_html_request(html_request):
         for td in tr.find_all('td'):
             columns.append(td.text)
         rows.append(columns)
+        
+    rows = rows[1:]
+    for row in rows:
+        year, month, domestic, international, _ = row
+        if month == 'TOTAL':
+            rows.remove(row)
     
     return rows
 
@@ -96,14 +99,13 @@ def get_combinations():
     
 def extract_data_to_json(airline, airport, international=False, create_file=False):
     
-    html_request = _extract_html(airline, airport)
+    html_requests = _extract_html(airline, airport)
     
     data = []
     info = {}
     info["courier"], info["airport"] = airline, airport
     
-    rows = _parse_html_request(html_request)
-    rows = rows[1:]
+    rows = _parse_html_request(html_requests)
     
     for row in rows:
         year, month, domestic, international, _ = row
@@ -132,40 +134,76 @@ def extract_data_to_json(airline, airport, international=False, create_file=Fals
     return data
 
 
-def extract_data_to_csv(airline, airport, international=False, create_file=False):
+def _parse_indexes(rows):
     
-    html_request = _extract_html(airline, airport)
     indexes = []
-    if international:
-        prep_dict = {'Domestic': list(), 'International': list()}
-    else:
-        prep_dict = {'Domestic': list()}
     
-    rows = _parse_html_request(html_request)
-    rows = rows[1:]
-        
     for row in rows:
         year, month, domestic, international, _ = row
-        if month == 'TOTAL':
-            continue
         timestring = '{}-{}'.format(year, month)
         index = datetime.strptime(timestring, '%Y-%m')
         indexes.append(index)
+        
+    return indexes
+
+def _parse_data(rows, label, international=False):
+    
+    if international:
+        prep_dict = {'{}_Domestic'.format(label): list(), '{}_International'.format(label): list()}
+    else:
+        prep_dict = {'{}_Domestic'.format(label): list()}
+    
+    for row in rows:
+        _, _, domestic_data, international_data, _ = row
         try:
-            prep_dict['Domestic'].append(int(domestic.replace(',', '')))
+            prep_dict['{}_Domestic'.format(label)].append(int(domestic_data.replace(',', '')))
         except ValueError:
-            prep_dict['Domestic'].append(np.nan)
+            prep_dict['{}_Domestic'.format(label)].append(np.nan)
         if international:
             try:
-                prep_dict['International'].append(int(international.replace(',', '')))
+                prep_dict['{}_International'.format(label)].append(int(international_data.replace(',', '')))
             except ValueError:
-                prep_dict['International'].append(np.nan)
+                prep_dict['{}_International'.format(label)].append(np.nan)
     
-    df = pd.DataFrame(prep_dict, index=indexes)
+    return prep_dict
+
+
+def extract_data_to_csv(airline, airport, additional_requests=None, international=False, create_file=False):
+    
+    airlines = get_airlines()
+    if airline not in airlines:
+        raise ValueError(airline + " is an invalid airline code. Run get_airlines() in an interpreter"
+                         "for a full list of valid airline codes.")
+    
+    
+    airports = get_airports()
+    if airport not in airports:
+        raise ValueError(airport + " is an invalid airport code. Run get_airports() in an interpreter"
+                         "for a full list of valid airport codes.")
+        
+    html_requests = _extract_html(airline, airport, additional_requests)
+    passenger_rows = _parse_html_request(html_requests[0])
+    indexes = _parse_indexes(passenger_rows)
+    parsed_data = _parse_data(passenger_rows, 'Passengers', international)
+    
+    if additional_requests:
+        possible_additional = ["Flights", "RPM", "ASM"]
+        if any(item not in possible_additional for item in additional_requests):
+            raise ValueError("additional_requests includes an invalid value. Possible values include:"
+                " 'Flights', 'RPM', 'ASM'. Must be passed as a list.")
+        for i, request in enumerate(additional_requests):
+            rows = _parse_html_request(html_requests[i + 1])
+            parsed_rows = _parse_data(rows, request, international)
+            parsed_data.update(parsed_rows)
+    
+    df = pd.DataFrame(parsed_data, index=indexes)
     
     if create_file:
+        if not os.path.isdir(datadir):
+            os.mkdir(datadir)
         with open(datadir + '/{}-{}.csv'.format(airline, airport), 'w') as outfile:
             df.to_csv(outfile, index_label='Date')
+        return None
     
     return df
     
