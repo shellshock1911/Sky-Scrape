09d1d4f3f7abc616a2c274d0d2a50d7132df2b73
Franklin Bradfield <fbradfield1@gmail.com>
2017-06-26 17:40:07 +0800

    Removed functions for unpickling objects
    * Didn't realize that sharing pickle objects is bad practice
    * Removed reference to them from aviation_parsing.py
    * Replaced them with utility functions in utilities.py
    * These return the desired lists directly instead of unpickling them
    * Removed get_combinations() as use case isn't clearly defined

---
+++ utilities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 17:17:13 2017
+
+@author: BennyBluebird
+"""
+
+def get_airlines():
+    
+    """Returns a list of all possible airline codes that can be later passed
+    as arguments to extract_data_to_json or extract_data_to_csv. Must be called 
+    from Flight-Forecast top-level directory.
+    
+    """
+    
+    airlines = ['AS', 'G4', 'AA', '5Y', 'DL', 'MQ', 'EV', 'F9', 'HA', 'B6', 'OO',
+                'WN', 'NK', 'UA', 'VX']
+        
+    return airlines
+
+def get_airports():
+    
+    """Returns a list of all possible airport codes that can be later passed
+    as arguments to extract_data_to_json or extract_data_to_csv. Must be called 
+    from Flight-Forecast top-level directory.
+    
+    """
+    
+    airports = ['ATL', 'BWI', 'BOS', 'CLT', 'MDW', 'ORD', 'DAL', 'DFW', 'DEN', 
+                'DTW', 'FLL', 'IAH', 'LAS', 'LAX', 'MIA', 'MSP', 'JFK', 'LGA', 
+                'EWR', 'MCO', 'PHL', 'PHX', 'PDX', 'SLC', 'SAN', 'SFO', 'SEA', 
+                'TPA', 'DCA', 'IAD']
+        
+    return airports
